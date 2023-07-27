520cd528fd377efb9fd35dfdf10f2f35c5b26138
Danish A <42688495+danisha138@users.noreply.github.com>
2023-07-27 07:38:25 +0530

    Update zip-unzip.sh

--- zip-unzip.sh
+++ zip-unzip.sh
@@ -1,3 +1,10 @@
+#zip and unzip .zip files
 unzip abc.zip -d tempdir
 
 zip -r -j abc.zip tempdi
+
+#zip file to .gz
+gzip -v filename.csv                OR           gzip -v9 filename.csv
+
+#unzip .gz file 
+gzip -d -k filename.csv.gz
